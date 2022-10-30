b0d557698c917dc21ad4a6c2ee50d842b67c1f61
Calumarii <callumjbeckett@outlook.com>
2022-10-30 17:08:29 +0100

    added nuget package newtonsoft

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("GameDb"));
 
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
